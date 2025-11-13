7b0c248990d68c89ddc4ea6e4d158b7e7ba4c6f7
Luan <114256871+Luan067@users.noreply.github.com>
2025-11-13 15:49:00 -0300

    Reorder imports in script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
-import initFaqAccordion from "./modules/accordion.js";
-import initScrollAnimation from "./modules/scroll-animation.js";
 import SmoothScroll from "./modules/scroll-suave.js";
+import initFaqAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabNav.js";
 import initModal from "./modules/modal.js";
 import initToolTip from "./modules/tooltip.js";
@@ -9,12 +8,12 @@ import initMobileMenu from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBtc from "./modules/fetch-btc.js";
+import initScrollAnimation from "./modules/scroll-animation.js";
 
 const smoothScroll = new SmoothScroll('[data-scroll="suave"] a[href^="#"]');
 smoothScroll.init();
 
 initFaqAccordion();
-initScrollAnimation();
 initTabNav();
 initModal();
 initToolTip();
@@ -23,3 +22,4 @@ initMobileMenu();
 initFuncionamento();
 initFetchAnimais();
 initFetchBtc();
+initScrollAnimation();
